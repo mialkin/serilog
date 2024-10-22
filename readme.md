32876a8a188925577f19c1fc0b3adfc855d5f943
Aleksei Mialkin <aleksei@mialkin.ru>
2024-10-22 13:32:40 +0300

    Upgrade application

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Serilog
+
+Showcase of [↑ Serilog](https://github.com/serilog/serilog) library.
+
+```bash
+dotnet run --project=src/Serilog.Api
+```
--- src/Serilog.Api/Program.cs
+++ src/Serilog.Api/Program.cs
@@ -1,36 +1,38 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-    configuration.WriteTo.Console();
-    // configuration.WriteTo.File("/Users/john.doe/Downloads/serilog-api.log");
-});
+builder.Host.UseSerilog(
+    (context, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(context.Configuration);
+        configuration.WriteTo.Console();
+        // configuration.WriteTo.File("/Users/john.doe/Downloads/serilog-api.log");
+    });
 
 var services = builder.Services;
 
-services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen(options => { options.DescribeAllParametersInCamelCase(); });
 
 services.AddRouting(options => options.LowercaseUrls = true);
 
-var app = builder.Build();
+var application = builder.Build();
 
-app.UseSerilogRequestLogging();
+application.UseSerilogRequestLogging();
 
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
+application.UseSwagger();
+application.UseSwaggerUI(
+    options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 
-app.UseRouting();
-app.MapControllers();
+application.UseRouting();
 
-app.Run();
+application.MapGet(
+    pattern: "say-hello",
+    handler: (ILogger<Program> logger) => logger.LogInformation("Hello"));
+
+application.Run();
